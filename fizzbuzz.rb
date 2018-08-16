a151321c248f1ef9b4ed29beb9ab3e6c5e18a9fe
braj011 <bhagyaraj01@gmail.com>
2018-08-16 15:25:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
-require 'pry'
+#require 'pry'
 
 def fizzbuzz(int)
-  binding.pry
+  #binding.pry
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz" 
   elsif int % 3 == 0
@@ -15,5 +15,5 @@ end
 
 fizzbuzz(25)
 
-# gem - essentially a library in Ruby, like the debugger in JS.
+# gem - essentially a library in Ruby, like the debugger#¡€#¢∞§¶ in JS.
 # google Pry Repo - there's a class called binding, binding.pry stops the code processing
